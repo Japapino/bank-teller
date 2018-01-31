8671016b4f77cf33e3ed5a93581de83e12e1670e
Jallappino <stefan.murakami@gmail.com>
2018-01-31 15:22:21 -0500

    first few tests

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,9 +1,37 @@
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Account {
+	String accountNum;
+	String name; 
+	Map<String,String> accounts = new HashMap<String, String>(); 
+	
 	
 	public Account(String accountNum, String name) {
-		
-		
+		this.accountNum = accountNum;
+		this.name = name;
 	}
+	
+	public int checkAccountList() {
+		return accounts.size(); 
+	}
+	
+	public void addAccount(String accountNum, String accountName) {
+		this.accountNum = accountNum; 
+		this.name = accountName; 
+		accounts.put(accountNum, accountName); 
+	}
+	
+	public String getName(String accountNum) {
+		return accounts.get(accountNum); 
+	}
+	
+	public String getNum(String accountName) {
+		return accounts.get(accountName); 
+	}
+	
+	
 
 }
--- src/test/java/accountTest.java
+++ src/test/java/accountTest.java
@@ -1,3 +1,6 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 
 public class accountTest {
@@ -5,6 +8,32 @@ public class accountTest {
 	public void shouldInstantiateAnAccount() {
 		Account underTest = new Account("",""); 
 		assertNotNull(underTest); 
+		
+	}
+	
+	@Test
+	public void shouldCheckForAccounts() {
+		Account underTest = new Account("","");
+		underTest.addAccount("1234","Billy Bob");
+		int check = underTest.checkAccountList();
+		assertEquals(1,check); 
+	}
+	
+	@Test
+	public void shouldReturnAccountName() {
+		Account underTest = new Account("",""); 
+		underTest.addAccount("1234", "Billy Bob");
+		String checkName = underTest.getName("1234");
+		assertEquals("Billy Bob", checkName);
+		
+	}
+	
+	@Test
+	public void shouldReturnAccountNumber() {
+		Account underTest = new Account("", "");
+		underTest.addAccount("1234", "Billy Bob");
+		String checkNum = underTest.getNum("Billy Bob");
+		assertEquals("1234", checkNum); 
 		
 	}
 
